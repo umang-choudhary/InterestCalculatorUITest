624bf3ddba6c9bd967b7f1b28b26ae1416a0bf7e
Jarrod <learntodroid1@gmail.com>
2019-11-02 13:15:05 +1100

    remove hardcoded data from financial results livedata

--- app/src/main/java/com/learntodroid/interestcalculator/repositories/FinancialResultRepository.java
+++ app/src/main/java/com/learntodroid/interestcalculator/repositories/FinancialResultRepository.java
@@ -19,17 +19,8 @@ public class FinancialResultRepository {
     }
 
     public MutableLiveData<List<FinancialResult>> getResults() {
-//        setResults();
-
         MutableLiveData<List<FinancialResult>> data = new MutableLiveData<>();
         data.setValue(dataSet);
         return data;
     }
-
-    // hardcoded data
-//    private void setResults() {
-//        dataSet.add(new FinancialResult(1, 1000));
-//        dataSet.add(new FinancialResult(2, 1005));
-//        dataSet.add(new FinancialResult(3, 1010));
-//    }
 }
